e9bd7ad31baf62ed2ae2e9eb2ed742f8474dbedd
goongju <35947738+goongju@users.noreply.github.com>
2020-01-12 18:21:39 +0900

    Add files via upload

---
+++ JoinString.kt
@@ -0,0 +1,17 @@
+import java.lang.StringBuilder
+
+fun <T> joinToString(collection:Collection<T>, seperator:String=",", prefix:String="", postfix:String=""):String{
+    val result=StringBuilder(prefix)
+    for ((index,element) in collection.withIndex()){
+        if (index>0) result.append(seperator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun main() {
+    val list = listOf(1, 2, 3)
+    println(joinToString(list))
+    println(joinToString(list,";","<",">"))
+}
