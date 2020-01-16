288852ce490a4e11e1e2f94a13a20ba3dac89908
goongju <35947738+goongju@users.noreply.github.com>
2020-01-16 19:56:58 +0900

    Add files via upload

---
+++ 10827.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc=Scanner(System.`in`)
+    val a=sc.nextBigDecimal()
+    val b=sc.nextInt()
+    val result=a.pow(b)
+    println(result.toPlainString())
+}
